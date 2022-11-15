b472066b247c19324fac26e35794c17b255eab51
Matheus Nunes <matheusnunes.sobrinho18@gmail.com>
2022-11-15 16:51:20 -0300

    initial commit

---
+++ src/main/java/com/easyserviceapi/EasyServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.easyserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyServiceApiApplication.class, args);
+	}
+
+}
