8fd2a951f617c51ef5049171d193573b5488fdc0
Matheus Nunes <matheusnunes.sobrinho18@gmail.com>
2022-11-18 08:53:17 -0300

    change the strategy to generate ids

--- src/main/java/com/easyserviceapi/models/UserModel.java
+++ src/main/java/com/easyserviceapi/models/UserModel.java
@@ -2,13 +2,12 @@ package com.easyserviceapi.models;
 
 import java.io.Serializable;
 
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Table(name = "TB_USER")
 @Entity
@@ -17,7 +16,7 @@ public class UserModel implements Serializable {
     private static final Long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, length = 70)
